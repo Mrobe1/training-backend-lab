9dfddfba98de1eaa9e8714a6272cd7941661afc8
mrobe1 <michael.roberts@cloudreach.com>
2022-04-12 20:57:18 -0400

    dynamo db tbl name change

--- s3.tf
+++ s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_versioning" "version_my_bucket" {
   }
 }
 resource "aws_dynamodb_table" "terraform_lock_tbl" {
-  name           = "terraform-lock"
+  name           = "terraform_lock_tbl"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
